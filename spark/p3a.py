e12645399252a7ced1639944f5f1b6d443bd5f32
irixavales <irixa.vales@upr.edu>
2018-10-24 23:20:43 -0400

    spark done

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,15 @@
 #Code here
+
+from pyspark.sql import SparkSession, SQLContext
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.read.format("csv").load("/home/ubuntu/eclipse-workspace/exam1-sp17-bigdata-desc/hive.studentsPR.csv")
+
+df.createOrReplaceTempView("estudiantes")
+
+df2 = spark.read.format("csv").load("/home/ubuntu/eclipse-workspace/exam1-sp17-bigdata-desc/hive.escuelasPR.csv")
+
+df2.createOrReplaceTempView("escuelas")
+
+spark.sql("select * from estudiantes natural join escuelas using(school_id) where (ciudad='Ponce' or ciudad='San Juan') and escuelas.nivel='Superior').show()
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,5 @@
 #Code here
+
+# this code follows the code implemented on p3a.py
+
+spark.sql("select distrito, ciudad, count(*) from escuelas where region='Arecibo').show()
